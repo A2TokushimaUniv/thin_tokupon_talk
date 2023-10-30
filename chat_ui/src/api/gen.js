7130975cbf413dfac77be6813a33055e3e3bfc91
hayamiti <c612335022@tokushima-u.ac.jp>
2023-10-30 12:45:44 +0900

    課題用にコード削除

--- chat_ui/src/api/gen.js
+++ chat_ui/src/api/gen.js
@@ -1,9 +1,3 @@
 const genAPI = (userInput, callback) => {
-    fetch(`https://${API_HOME}/gen?text=${userInput}`, {
-        headers: {
-            'ngrok-skip-browser-warning': 'true'
-        },
-        method: 'GET',
-    }).then((response) => response.text())
-    .then(callback)
+
 }
